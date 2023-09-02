7e2394e79f55da25f6d42b27a9288542eef8acaf
BarelyAnXer <christiangazzingan38@gmail.com>
2023-09-02 13:55:31 +0800

    add changed database config

--- server/config/dbConn.js
+++ server/config/dbConn.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        // await mongoose.connect(process.env.DATABASE_URI)
-        await mongoose.connect("mongodb://127.0.0.1:27017/database")
+        await mongoose.connect(process.env.DATABASE_URI)
+        // await mongoose.connect("mongodb://127.0.0.1:27017/database")
         console.log("connected")
     } catch (err) {
         console.log(err)
